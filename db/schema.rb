6288674d566e2d5cd9322c7e7d66f15492aca92c
mbyk <marubayashi@gmail.com>
2020-03-31 17:27:48 +0900

    deviseの初期設定

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,3 @@
-# frozen_string_literal: true
-
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -12,7 +10,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_25_102206) do
+ActiveRecord::Schema.define(version: 2020_03_31_082433) do
+
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "memo"
@@ -21,4 +20,13 @@ ActiveRecord::Schema.define(version: 2020_02_25_102206) do
     t.string "author"
     t.string "picture"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+  end
+
 end
