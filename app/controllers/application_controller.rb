22983938c621220a5cd32bf1043c744b783f3d32
mbyk <marubayashi@gmail.com>
2020-04-01 21:09:59 +0900

    コントローラに必須ログイン処理を追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+
+    before_action :authenticate_user!
+
+    def after_sign_in_path_for(resource)
+        "/books"
+    end
 end
