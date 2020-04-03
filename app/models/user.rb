f6b5b788ff214176eff50f27b2b6407bef6d6ca9
mbyk <marubayashi@gmail.com>
2020-04-03 19:31:40 +0900

    郵便番号のバリデーションを追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,18 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :validatable
+
+  attr_accessor :forward_postcode, :backward_postcode 
+
+  validates :forward_postcode, presence: true
+  validates :backward_postcode, presence: true
+  validates :postcode, format: { with: /\A\d{3}-\d{4}\Z/ }
+
+  before_validation  :validate_postcode
+
+  private
+
+  def validate_postcode
+    self.postcode = "#{forward_postcode}-#{backward_postcode}"
+  end
 end
