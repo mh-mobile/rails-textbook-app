7f5bd94254988328a86b72f9ccc1119ca02455bc
mbyk <marubayashi@gmail.com>
2020-04-03 16:41:47 +0900

    住所のマイグレーションを追加

---
+++ db/migrate/20200403062930_add_address_to_users.rb
@@ -0,0 +1,10 @@
+class AddAddressToUsers < ActiveRecord::Migration[6.0]
+  def up
+    add_column :users, :address, :string
+    change_column :users, :address, :string, null: false
+  end
+
+  def down
+    remove_column :users, :address
+  end
+end
