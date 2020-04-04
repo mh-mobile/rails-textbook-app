853ab1076602a9687caaf0283c50586e69eb757c
mbyk <marubayashi@gmail.com>
2020-04-04 17:43:10 +0900

    ログイン後の遷移先のパスをヘルパーパスに置き換え

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,8 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
 
+  # ログイン後の遷移先
   def after_sign_in_path_for(resource)
-    "/books"
+    authenticated_root_path
   end
 end
