6a03f16fc4e9a6605fccad3111688d48abd82455
mbyk <marubayashi@gmail.com>
2020-04-04 17:17:35 +0900

    プロフィール更新時のパラメータ設定の不備を修正

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -42,12 +42,12 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_sign_up_params
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :address, :email, :self_introduction, :password, :password_confirmation, :forward_postcode, :backward_postcode])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email,  :password, :password_confirmation])
   end
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email,  :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :address, :email, :self_introduction, :password, :password_confirmation, :forward_postcode, :backward_postcode])
   end
 
   # The path used after sign up.
