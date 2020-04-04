2d7210098625642d60858a430275018ab486080f
mbyk <marubayashi@gmail.com>
2020-04-04 17:37:21 +0900

    正規表現のコメントを追加

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
-ROUTING_USERNAME = REGEX = /([a-z\d]+-)*[a-z\d]+/
+# ルーティングのユーザー名の正規表現
+ROUTING_USERNAME = /([a-z\d]+-)*[a-z\d]+/
+
+# ユーザーモデルのユーザー名の正規表現
 USERMODEL_USERNAME_REGEX = /\A([a-z\d]+-)*[a-z\d]+\Z/
+
+# ユーザーモデルの郵便番号の正規表現
 USERMODEL_POSTCODE_REGEX = /\A\d{3}-\d{4}\Z/
