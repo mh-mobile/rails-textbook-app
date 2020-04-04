4dacaf137ec12f2158e4840ce1023ffa06b14e0a
mbyk <marubayashi@gmail.com>
2020-04-04 15:15:35 +0900

    ログイン時に不要なパラメータを削除

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -47,7 +47,7 @@ class Users::RegistrationsController < Devise::RegistrationsController
 
   # If you have extra params to permit, append them to the sanitizer.
   def configure_account_update_params
-    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :address, :email, :self_introduction, :password, :password_confirmation, :forward_postcode, :backward_postcode])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :email,  :password, :password_confirmation])
   end
 
   # The path used after sign up.
