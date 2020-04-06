f621b758717382f7ab5e6594c004dce0751b5737
mbyk <marubayashi@gmail.com>
2020-04-06 13:07:02 +0900

    レビュー対応: 正規表現の定数を修正

--- config/initializers/constants.rb
+++ config/initializers/constants.rb
@@ -1,10 +1,4 @@
 # frozen_string_literal: true
 
 # ルーティングのユーザー名の正規表現
-ROUTING_USERNAME = /([a-z\d]+-)*[a-z\d]+/
-
-# ユーザーモデルのユーザー名の正規表現
-USERMODEL_USERNAME_REGEX = /\A([a-z\d]+-)*[a-z\d]+\Z/
-
-# ユーザーモデルの郵便番号の正規表現
-USERMODEL_POSTCODE_REGEX = /\A\d{3}-\d{4}\Z/
+ROUTING_USERNAME = /([a-z\d]+-)*[a-z\d]+/.freeze
