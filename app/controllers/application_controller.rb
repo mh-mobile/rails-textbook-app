560e8c9156081164a7a0508cd670bd5a825b0fb9
mbyk <marubayashi@gmail.com>
2020-04-07 20:30:40 +0900

    Userモデルのデコレータでプロフィールアイコンの表示処理を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,10 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     authenticated_root_path
   end
+
+  # current_userでデコレータを使えるようする。
+  # ref. https://github.com/amatsuda/active_decorator/issues/98
+  def current_user
+    (user = super) && ActiveDecorator::Decorator.instance.decorate(user)
+  end
 end
