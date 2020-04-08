e46991aacd8e2de1d9f9458fd65808625bcfe338
mbyk <marubayashi@gmail.com>
2020-04-08 23:37:56 +0900

    rubocopを適用

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -1,16 +1,15 @@
 # frozen_string_literal: true
 
 module UserDecorator
-  PROFILE_ICON_CLASS_NAME = "profile_icon".freeze
+  PROFILE_ICON_CLASS_NAME = "profile_icon"
 
   # プロフィールアイコンを表示する
   # アイコンが存在しない場合は、デフォルトのアイコンを表示する
   def profile_icon_tag
-    if profile_icon.attached? 
-      image_tag profile_icon, class: PROFILE_ICON_CLASS_NAME 
+    if profile_icon.attached?
+      image_tag profile_icon, class: PROFILE_ICON_CLASS_NAME
     else
       image_tag "/assets/person_noimage.png", class: PROFILE_ICON_CLASS_NAME
     end
   end
-  
 end
