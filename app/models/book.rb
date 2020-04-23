225f1a439c6c0383d6b434aec7c2f964f5c1aa4d
mbyk <marubayashi@gmail.com>
2020-04-23 23:52:33 +0900

    書籍のフィード取得処理の実装

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,4 +3,11 @@
 class Book < ApplicationRecord
   mount_uploader :picture, PictureUploader
   belongs_to :user
+
+  default_scope -> { order("created_at DESC") }
+
+  def self.following_feeds(user)
+    following_ids_sql = "SELECT followed_id from friendships WHERE follower_id = :user_id"
+    where("user_id IN (#{following_ids_sql}) OR user_id = :user_id", user_id: user.id)
+  end
 end
