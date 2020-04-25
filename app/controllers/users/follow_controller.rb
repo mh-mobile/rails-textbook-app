68fd5e3490da53f204c6cc798b239214d5032d1c
mbyk <marubayashi@gmail.com>
2020-04-25 15:18:09 +0900

    フォロー処理の共通化

--- app/controllers/users/follow_controller.rb
+++ app/controllers/users/follow_controller.rb
@@ -2,18 +2,33 @@ class Users::FollowController < ApplicationController
   before_action :set_user
 
   def follow
-    return head :no_content if current_user == @user
-    current_user.follow!(@user) unless current_user.following?(@user)
-    render partial: "users/shared/follow", locals: { user: @user }
+    follow_action(:follow!)
   end
 
   def unfollow
-    return head :no_content if current_user == @user
-    current_user.unfollow!(@user) if current_user.following?(@user)
-    render partial: "users/shared/follow", locals: { user: @user }
+    follow_action(:unfollow!)
   end
 
   private
+
+  def follow_action(name)
+    return head :no_content if current_user == @user
+
+    if canFollow?(name) || canUnfollow?(name)
+      current_user.send(name, @user)
+    end
+
+    render partial: "users/shared/follow", locals: { user: @user }
+  end
+
+  def canFollow?(name)
+    name == :follow! && !current_user.following?(@user)
+  end
+
+  def canUnfollow?(name)
+    name == :unfollow! && current_user.following?(@user)
+  end
+ 
 
   def set_user
     username = params[:username]
