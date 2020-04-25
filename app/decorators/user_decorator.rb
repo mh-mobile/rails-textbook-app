df63c82e6de6a64e13eb65f983bd2a6e1f2682c2
mbyk <marubayashi@gmail.com>
2020-04-25 14:16:21 +0900

    フォローのAjax対応

--- app/decorators/user_decorator.rb
+++ app/decorators/user_decorator.rb
@@ -12,4 +12,29 @@ module UserDecorator
       image_tag "/assets/person_noimage.png", class: PROFILE_ICON_CLASS_NAME
     end
   end
+
+  # フォロー用のタグを生成する。
+  # 既にフォロー済みのボタンは、フォロー解除のリクエストを発行するFollowingボタンを表示する。
+  # フォローしていない場合は、フォローのリクエストを発行するFollowボタンを表示する。
+  def follow_tag(user)
+    id_names = ["user-#{user.id}"]
+    if following?(user)
+      title = "Following"
+      path = unfollow_user_path(username: user.username)
+      class_names = ["button", "is-link", "is-active"]
+      method = :delete
+    else
+      title = "Follow"
+      path = follow_user_path(username: user.username)
+      class_names = ["button", "is-light"]
+      method = :post
+    end
+
+    # フォロー・アンフォローにリンク生成
+    follow_link = link_to(title, path, method: method, remote: true, class: class_names)
+
+    # divのid属性付きでリンクを返却
+    content_tag(:div, follow_link, id: id_names)
+  end
+
 end
