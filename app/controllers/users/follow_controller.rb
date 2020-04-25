37d1e17bddc7f75197773d56ef18d6712f76d064
mbyk <marubayashi@gmail.com>
2020-04-25 14:16:21 +0900

    フォローのAjax対応

--- app/controllers/users/follow_controller.rb
+++ app/controllers/users/follow_controller.rb
@@ -1,29 +1,14 @@
 class Users::FollowController < ApplicationController
-  before_action :set_user, :set_redirect_path
-
-  # フォローのリクエスト種別
-  # ユーザリストからのリクエストはusers
-  # プロフィールからのリクエストはprofile
-  # フォロワー画面からのリクエストはprofile
-  # フォロー画面からのリクエストはprofile
-  module RequestType
-    USERS = "users".freeze
-    PROFILE = "profile".freeze
-    FOLLOWERS = "followers".freeze
-    FOLLOWING = "following".freeze
-  end
-  RequestType.freeze
+  before_action :set_user
 
   def follow
-    return redirect_to @redirect_path if current_user == @user
+    return head :no_content if current_user == @user
     current_user.follow!(@user) unless current_user.following?(@user)
-    redirect_to @redirect_path, notice: "フォローしました"
   end
 
   def unfollow
-    return redirect_to @redirect_path if current_user == @user
+    return head :no_content if current_user == @user
     current_user.unfollow!(@user) if current_user.following?(@user)
-    redirect_to @redirect_path, notice: "フォローを解除しました"
   end
 
   private
@@ -32,25 +17,6 @@ class Users::FollowController < ApplicationController
     username = params[:username]
 
     @user = User.find_by!(username: username)
-  end
-
-  def set_redirect_path
-    request_type = follow_params[:request_type]
-
-    case request_type
-    when RequestType::PROFILE 
-      @redirect_path = user_path(username: @user.username)
-    when RequestType::USERS
-      @redirect_path = user_index_path
-    when RequestType::FOLLOWING
-      @redirect_path = following_user_path
-    when RequestType::FOLLOWERS
-      @redirect_path = followers_user_path
-    end
-  end
-
-  def follow_params
-    params.require(:follow).permit(:request_type)
   end
 
 end
