a7b42aa9c785bd3fb0ca7fd22efa6a1ecd8cad42
mbyk <marubayashi@gmail.com>
2020-05-03 16:55:00 +0900

    FollowingControllerにフォロー処理やフォロー解除のアクションを移動

--- app/controllers/users/following_controller.rb
+++ app/controllers/users/following_controller.rb
@@ -1,9 +1,24 @@
 # frozen_string_literal: true
 
 class Users::FollowingController < ApplicationController
-  before_action :set_user, only: [:index]
+  before_action :set_user
 
   def index
     @users = @user.following.page(params[:page])
   end
+
+  def create
+    follow_action(:follow!)
+  end
+
+  def destroy
+    follow_action(:unfollow!)
+  end
+
+  private
+    def follow_action(name)
+      return head :no_content if current_user == @user
+      current_user.send(name, @user) if current_user.can_follow_action?(name, @user)
+      render partial: "users/shared/follow", locals: { user: @user }
+    end
 end
