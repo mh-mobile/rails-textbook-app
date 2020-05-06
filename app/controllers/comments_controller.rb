38387551cd185fab6529f8af09bb320493fce153
mbyk <marubayashi@gmail.com>
2020-05-06 22:11:42 +0900

    CommentControllerでcommentableを取得する処理を追加

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class CommentsController < ApplicationController
+  before_action :set_commentale
 
   def update
   end
@@ -9,5 +10,13 @@ class CommentsController < ApplicationController
   end
 
   def create
+
   end
+
+  private 
+
+    def set_commentable
+      @commentable = params[:commentable_type].constantize.find(param[:commentable_id])
+    end
+
 end
