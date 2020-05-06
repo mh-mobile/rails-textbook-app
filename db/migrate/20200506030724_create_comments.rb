70b9c51bdf2548815644e769e3c6b340f22af843
mbyk <marubayashi@gmail.com>
2020-05-06 12:11:08 +0900

    コメントのマイグレーションを作成

---
+++ db/migrate/20200506030724_create_comments.rb
@@ -0,0 +1,10 @@
+class CreateComments < ActiveRecord::Migration[6.0]
+  def change
+    create_table :comments do |t|
+      t.string :content
+      t.belongs_to :commentable, polymorphic: true
+      t.belongs_to :user
+      t.timestamps
+    end
+  end
+end
