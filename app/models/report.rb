947d14a45f06e82d5e07e7b223759c91a55a8c29
mbyk <marubayashi@gmail.com>
2020-05-08 18:06:08 +0900

    コメントと日報のバリデーションを追加

--- app/models/report.rb
+++ app/models/report.rb
@@ -5,4 +5,8 @@ class Report < ApplicationRecord
   belongs_to :user
 
   default_scope -> { order("created_at DESC") }
+
+  validates :title, presence: true
+  validates :learning_date, presence: true, uniqueness: { scope: :user, message: "指定の日付の日報を既に作成済みです" }
+  validates :description, presence: true
 end
