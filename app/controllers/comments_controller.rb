5288133b03fcf72f187a03632ac0efa7b18f5a85
mbyk <marubayashi@gmail.com>
2020-05-08 14:31:29 +0900

    他人のコメントは編集できないように制御

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,6 +3,9 @@
 class CommentsController < ApplicationController
   before_action :set_commentable, only: [:create, :update]
   before_action :set_comment, only: [:update, :destroy]
+  before_action -> {
+    check_permission(@comment.user)
+  }, only: [:update, :destroy]
 
   def update
     unless @comment.update(comment_params)
