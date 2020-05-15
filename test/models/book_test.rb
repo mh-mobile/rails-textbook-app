e5d4ee6e668676d7ab81ed179ca96a2a8a6585d0
mbyk <marubayashi@gmail.com>
2020-05-16 05:04:58 +0900

    Bookモデルのモデルテストを追加

--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,7 +1,24 @@
-require 'test_helper'
+# frozen_string_literal: true
+
+require "test_helper"
 
 class BookTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should belongs_to corrent user" do
+    mh_mobile = users(:mh_mobile)
+    book_1 = books(:book_1)
+
+    assert_equal mh_mobile, book_1.user
+  end
+
+  test "#save" do
+    mh_mobile = users(:mh_mobile)
+    book = Book.new(title: "タイトル",
+        author: "著者",
+        memo: "メモ",
+        user: mh_mobile,
+        created_at: "2020-01-06 00:00:0"
+      )
+    book.save
+    assert book["id"].present?
+  end
 end
