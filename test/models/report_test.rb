f9ca8ffce3e7d3044765eca74e0341863dae918a
mbyk <marubayashi@gmail.com>
2020-05-16 11:45:58 +0900

    日報のモデルテストを追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -3,7 +3,22 @@
 require "test_helper"
 
 class ReportTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should belongs_to corrent user" do
+    mh_mobile = users(:mh_mobile)
+    report_1 = reports(:report_1)
+
+    assert_equal mh_mobile, report_1.user
+  end
+
+  test "#save" do
+    mh_mobile = users(:mh_mobile)
+    report = Report.new(
+      title: "日報を提出",
+      description: "日報の内容",
+      learning_date: Date.current,
+      user: mh_mobile
+      )
+    report.save
+    assert report["id"].present?
+  end
 end
