f4f2c9691a4df82f23a93960508e0ac483903890
mbyk <marubayashi@gmail.com>
2020-05-16 12:30:42 +0900

    コメントのモデルテストを追加

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -3,7 +3,40 @@
 require "test_helper"
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "should belongs_to corrent user" do
+    hiro = users(:hiro)
+    comment_1 = comments(:comment_1)
+
+    assert_equal hiro, comment_1.user
+  end
+
+  test "#save" do
+    mh_mobile = users(:mh_mobile)
+    book_1 = books(:book_1)
+    comment = Comment.new(
+      content: "書籍コメント投稿",
+      commentable: book_1,
+      commentable_type: "Book",
+      )
+    comment.user = mh_mobile
+    comment.save
+    assert comment["id"].present?
+  end
+
+  test "should belongs_to corrent commentable" do
+    book_1 = books(:book_1)
+    comment_1 = comments(:comment_1)
+    commentable = comment_1.commentable
+
+    assert_equal book_1, commentable
+  end
+
+  test "commented users" do
+    report_1 = reports(:report_1)
+    hiro = users(:hiro)
+    hanako = users(:hanako)
+    mh_mobile = users(:mh_mobile)
+    commented_users = report_1.comments.map(&:user).uniq
+    assert_equal [hiro, hanako, mh_mobile], commented_users
+  end
 end
