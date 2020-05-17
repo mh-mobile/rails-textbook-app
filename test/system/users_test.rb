ab6aa5c54e5ae9aec8041e54723480c9d4c30cc6
mbyk <marubayashi@gmail.com>
2020-05-17 17:35:27 +0900

    ユーザーリストのシステムテストを追加

--- test/system/users_test.rb
+++ test/system/users_test.rb
@@ -3,9 +3,26 @@
 require "application_system_test_case"
 
 class UsersTest < ApplicationSystemTestCase
+  setup { login_user "mh-mobile", "QjT7QeyjUDUYz2Fy" }
+
   test "show listing users" do
+    mh_mobile = users(:mh_mobile)
+    hiro = users(:hiro)
+    hanako = users(:hanako)
+    taro = users(:taro)
+
+    visit "/users"
+    assert_text "ユーザーリスト"
+    assert_text hiro.username
+    assert_text hanako.username
+    assert_text taro.username
+    assert_no_text mh_mobile.username
   end
 
   test "show user" do
+    hiro = users(:hiro)
+    visit "/users/#{hiro.username}"
+    assert_text "プロフィール"
+    assert_text hiro.username
   end
 end
