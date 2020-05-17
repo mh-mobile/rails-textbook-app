5df12a98a9e8274f6862643dab49bba7d29a6aa6
mbyk <marubayashi@gmail.com>
2020-05-17 12:07:23 +0900

    HomesControllerのシステムテストを追加

--- test/system/homes_test.rb
+++ test/system/homes_test.rb
@@ -4,5 +4,9 @@ require "application_system_test_case"
 
 class HomesTest < ApplicationSystemTestCase
   test "show top page" do
+    visit "/"
+    assert_text "BookAppへようこそ"
+    assert_link "ログイン"
+    assert_link "サインアップ"
   end
 end
