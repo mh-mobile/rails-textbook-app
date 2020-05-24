d6c62626b60c23c12c08e4d1b7d4f2dd0cf3592d
mbyk <marubayashi@gmail.com>
2020-05-24 14:52:13 +0900

    deploy

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -5,7 +5,7 @@
    roles: %w{web app db},
    ssh_options: {
      port: 54321,
-     keys: %w(ENV["CIRCLE_RSA"]),
+     keys: %w(~/.ssh/id_rsa_d221d18afc86adcb1fe3331f5350bb88),
      forward_agent: true,
      auth_methods: %w(publickey)
    }
