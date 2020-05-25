987a1a466b76a3a83b3f3707d1ac47425bd7a0e6
mbyk <marubayashi@gmail.com>
2020-05-25 17:11:31 +0900

    LoginHelperをrequieで読み込むように変更

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 require "test_helper"
-require_relative "./supports/login_helper"
+require "supports/login_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   include LoginHelper
