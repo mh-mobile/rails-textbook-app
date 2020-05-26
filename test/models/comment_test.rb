a4584a4412d906c449f4899bbd7cd54de24f9be8
mbyk <marubayashi@gmail.com>
2020-05-26 11:06:53 +0900

    Commentモデルに独自に実装しているメソッドがないので、Commentモデルテストを削除

--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -3,21 +3,6 @@
 require "test_helper"
 
 class CommentTest < ActiveSupport::TestCase
-  test "should belongs_to corrent user" do
-    hiro = users(:hiro)
-    comment_1 = comments(:comment_1)
-
-    assert_equal hiro, comment_1.user
-  end
-
-  test "should belongs_to corrent commentable" do
-    book_1 = books(:book_1)
-    comment_1 = comments(:comment_1)
-    commentable = comment_1.commentable
-
-    assert_equal book_1, commentable
-  end
-
   test "commented users" do
     report_1 = reports(:report_1)
     hiro = users(:hiro)
